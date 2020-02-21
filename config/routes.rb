121803f478c9d1e3488a9a13cce9e869c74d719f
jorge-Coke <jorge.barra1412@gmail.com>
2020-02-21 18:48:27 -0300

    controller stories created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-
+  get 'stories/index'
+  get 'stories/show'
+  resources :stories
   devise_for :users
-  root to: "home#index"
+
+  root to: "stories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
